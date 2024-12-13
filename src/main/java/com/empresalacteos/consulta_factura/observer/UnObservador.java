ab1719e00e9040ab76955cadd538cb128d5256dc
jeanpaul119 <150830134+jeanpaul119@users.noreply.github.com>
2024-12-12 19:32:36 -0500

    actualizacion patron observer

--- src/main/java/com/empresalacteos/consulta_factura/observer/UnObservador.java
+++ src/main/java/com/empresalacteos/consulta_factura/observer/UnObservador.java
@@ -3,20 +3,18 @@ package com.empresalacteos.consulta_factura.observer;
 public class UnObservador implements IObservador
 {
     private String nombre;
-    // ---------------------------------
     public UnObservador(String nombre) {
         this.setNombre(nombre);
         System.out.println("Observador [" + this.nombre + "] creado");
     }
-    // ---------------------------------
     public String getNombre() {
         return this.nombre;
     }
-    // ---------------------------------
+   
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    // ---------------------------------
+  
     @Override
      public void observadoActualizado() {
        System.out.println("Observador [" + this.getNombre() + "] recibe la notificación");
