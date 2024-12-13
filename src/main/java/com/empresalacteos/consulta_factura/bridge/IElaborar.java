4645dd46d5c59683b0f94ad2a1d2b22e1734fb11
jeanpaul119 <150830134+jeanpaul119@users.noreply.github.com>
2024-12-12 20:57:48 -0500

    restore

--- src/main/java/com/empresalacteos/consulta_factura/bridge/IElaborar.java
+++
@@ -1,5 +0,0 @@
-package com.empresalacteos.consulta_factura.bridge;
-
-public interface IElaborar {
-	public void procesar();
-}
--- src/main/java/com/empresalacteos/consulta_factura/composite/IPrecio.java
+++
@@ -1,6 +0,0 @@
-package com.empresalacteos.consulta_factura.composite;
-
-public interface IPrecio {
-	double getImporteTotal();
-
-}
